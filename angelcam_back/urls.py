aaf32fdbeafe2604328fea904c1b3d1b62021a7f
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2024-08-09 16:59:59 -0500

    Show live video development

--- angelcam_back/urls.py
+++ angelcam_back/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from cameras.views import home, login, get_cameras
+from cameras.views import home, login, get_cameras, get_camera_stream
 
 urlpatterns = [
     path('', home),
     path('api/login/', login),
     path('api/cameras/', get_cameras),
+    path('api/stream/<int:camera_id>/', get_camera_stream),
     path('admin/', admin.site.urls)
 ]
--- cameras/views.py
+++ cameras/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+from django.conf import settings
 
 def home(request):
     return JsonResponse({'message': 'Welcome to API!'})
@@ -37,5 +38,34 @@ def get_cameras(request):
             return JsonResponse({'error': 'Failed to fetch cameras', 'details': response.json()}, status=401)
 
         return JsonResponse({'cameras': response.json()})
+
+    return JsonResponse({'error': 'Only GET requests are allowed'}, status=405)
+
+@csrf_exempt
+def get_camera_stream(request, camera_id):
+    if request.method == 'GET':
+        token = request.headers.get('Authorization', '').split(' ')[-1]
+
+        if not token:
+            return JsonResponse({'error': 'Authorization token is required'}, status=400)
+
+        url = f"https://api.angelcam.com/v1/shared-cameras/{camera_id}/"
+        headers = {'Authorization': f'PersonalAccessToken {token}'}
+        response = requests.get(url, headers=headers)
+        
+        if response.status_code == 200:
+            data = response.json()
+            stream_details = [
+                {'url': stream['url'], 'format': stream['format']}
+                for stream in data.get('streams', [])
+                if 'url' in stream and 'format' in stream
+            ]
+
+            if stream_details:
+                return JsonResponse({'stream_details': stream_details})
+            else:
+                return JsonResponse({'error': 'No streams available'}, status=404)
+        else:
+            return JsonResponse({'error': 'Could not fetch camera data', 'details': response.json()}, status=response.status_code)
 
     return JsonResponse({'error': 'Only GET requests are allowed'}, status=405)
